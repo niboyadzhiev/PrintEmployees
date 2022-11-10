86f706d75aa6f22b1fc8aed7aa06717d9d644084
niboyadzhiv <nikolay.ivanov.boyadzhiev@gmail.com>
2022-11-10 22:12:21 +0200

    new print method

--- src/main/java/PrintEmployee.java
+++ src/main/java/PrintEmployee.java
@@ -3,7 +3,20 @@ import java.sql.*;
 public class PrintEmployee {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ResultSet rs = getEmployees();
+        print(getEmployees());
+
+    }
+
+
+    public static ResultSet getEmployees () throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.log.Slf4JLogger");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/hr?" + "user=root&password=123456789");
+        Statement statement = connection.createStatement();
+        return  statement.executeQuery("SELECT * FROM employees");
+
+    }
+
+    public static void print (ResultSet rs) throws SQLException {
         int colNum = rs.getMetaData().getColumnCount();
         for (int i = 1; i <= colNum; i++ ) {
             if (i != colNum) {
@@ -23,15 +36,6 @@ public class PrintEmployee {
             }
             System.out.println();
         }
-
-    }
-
-
-    public static ResultSet getEmployees () throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.log.Slf4JLogger");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/hr?" + "user=root&password=123456789");
-        Statement statement = connection.createStatement();
-        return  statement.executeQuery("SELECT * FROM employees");
 
     }
 }
